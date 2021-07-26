64d271a8260dcac0b1dd55599a2a2815d733e781
Franklin Byaruhanga <byaruhaf@appbantu.com>
2021-07-26 04:39:29 +0300

    Bumps minimum iOS deployment target to iOS 9 because of Xcode 12
    Signed-off-by: Franklin Byaruhanga <byaruhaf@appbantu.com>

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "MKRingProgressView",
-    platforms: [.iOS(.v8), .tvOS(.v9)],
+    platforms: [.iOS(.v9), .tvOS(.v9)],
     products: [
         .library(name: "MKRingProgressView", targets: ["MKRingProgressView"])
     ],
